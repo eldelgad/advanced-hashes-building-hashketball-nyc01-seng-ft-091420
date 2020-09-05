0a73b456cdfd3131efedc8c1e0452b1d96d4cb44
Elias Delgado <evdelgado99@gmail.com>
2020-09-05 22:03:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,6 +5,7 @@ def game_hash
        team_name: "Brooklyn Nets",
        colors: ["Black","White"],
        players: [
+         
          {player_name: "Alan Anderson",
          number: 0,
          shoe: 16,
@@ -57,6 +58,7 @@ def game_hash
          ]},
          
     :away => {
+      
        team_name: "Charlotte Hotnets",
        colors: ["Turquoise","Purple"],
        players: [{
@@ -114,114 +116,3 @@ def game_hash
   }
   
 end
-
-# { :home
-  #   :team_name=> "Brooklyn Nets",
-  #   :colors=> "Black","White"
-  #   :players=> [{
-  #     player_name: "Alan Anderson", 
-  #     number: 0,
-  #     shoe: 16,
-  #     points: 22,
-  #     rebounds: 12,
-  #     assists: 12,
-  #     steals: 3,
-  #     blocks: 1,
-  #     slam_dunks: 1},
-      
-      # {player_name: "Reggie Evans", 
-      # number: 30,
-      # shoe: 14,
-      # points: 12,
-      # rebounds: 12,
-      # assists: 12,
-      # steals: 12,
-      # blocks: 12,
-      # slam_dunks: 7},
-      
-  #     {player_name: "Brook Lopez", 
-  #     number: 11,
-  #     shoe: 17,
-  #     points: 17,
-  #     rebounds: 19,
-  #     assists: 10,
-  #     steals: 3,
-  #     blocks: 1,
-  #     slam_dunks: 15},
-    
-  #     {player_name: "Mason Plunmlee", 
-  #     number: 1,
-  #     shoe: 19,
-  #     points: 26,
-  #     rebounds: 11,
-  #     assists: 6,
-  #     steals: 3,
-  #     blocks: 8,
-  #     slam_dunks: 5},
-      
-  #     {player_name: "Jason Terry", 
-  #     number: 31,
-  #     shoe: 15,
-  #     points: 19,
-  #     rebounds: 2,
-  #     assists: 2,
-  #     steals: 4,
-  #     blocks: 11,
-  #     slam_dunks: 1},
-  #     ]
-  # },
-  # :away=> {
-  #   :team_name=> "Charlotte Hotnets",
-  #   :colors=> ["Turquoise","Purple"],
-  #   :players=> [
-      # player_name: "Alan Anderson", 
-      # number: 0,
-      # shoe: 16,
-      # points: 22,
-      # rebounds: 12,
-      # assists: 12,
-      # steals: 3,
-      # blocks: 1,
-      # slam_dunks: 1},
-      
-      # {player_name: "Reggie Evans", 
-      # number: 30,
-      # shoe: 14,
-      # points: 12,
-      # rebounds: 12,
-      # assists: 12,
-      # steals: 12,
-      # blocks: 12,
-      # slam_dunks: 7},
-      
-      # {player_name: "Brook Lopez", 
-      # number: 11,
-      # shoe: 17,
-      # points: 17,
-      # rebounds: 19,
-      # assists: 10,
-      # steals: 3,
-      # blocks: 1,
-      # slam_dunks: 15},
-    
-      # {player_name: "Mason Plunmlee", 
-      # number: 1,
-      # shoe: 19,
-      # points: 26,
-      # rebounds: 11,
-      # assists: 6,
-      # steals: 3,
-      # blocks: 8,
-      # slam_dunks: 5},
-      
-      # {player_name: "Jason Terry", 
-      # number: 31,
-      # shoe: 15,
-      # points: 19,
-      # rebounds: 2,
-      # assists: 2,
-      # steals: 4,
-      # blocks: 11,
-      # slam_dunks: 1},
-  #     ]
-  # }
