87bfeca18d92ab4f60605aa4b46917392fe1d1ae
Elias Delgado <evdelgado99@gmail.com>
2020-09-05 21:45:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -11,14 +11,15 @@ def game_hash
        colors: ["Turquoise","Purple"],
        players: [{
          player_name: "Alan Anderson", 
-  #     number: 0,
-  #     shoe: 16,
-  #     points: 22,
-  #     rebounds: 12,
-  #     assists: 12,
-  #     steals: 3,
-  #     blocks: 1,
-  #     slam_dunks: 1}]
+         number: 0,
+         shoe: 16,
+         points: 22,
+         rebounds: 12,
+         assists: 12,
+         steals: 3,
+         blocks: 1,
+         slam_dunks: 1
+       }]
     }
   }
   
